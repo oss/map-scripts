5405ce3bedf68d4fdf309755a7e3666d6325fbbf
Matt Robinson <mattro@oit.rutgers.edu>
2016-02-25 16:11:25 -0500

    Cleanup and moving stuff to python
    I think eventually now that I know what I'm doing (for the most part) it
    will make more sense to kind of get everything into one python file.

--- dbtest.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from osm import osm2pgsql
-
-engine = create_engine('postgresql:///gis')
-
-Session = sessionmaker(bind=engine)
-session = Session()
